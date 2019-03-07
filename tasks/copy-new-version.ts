ecdd08fe7d3c646d477e8f2d69fa978162f01cc1
Frederique Mittelstaedt <frederique@automorph.com>
2019-03-07 14:52:39 +0000

    build: add custom semantic-release step for updating the package version

--- tasks/copy-new-version.ts
+++ tasks/copy-new-version.ts
@@ -1,5 +1,3 @@
-// tslint:disable:object-literal-sort-keys
-
 import fs from "fs";
 import path from "path";
 import prettier from "prettier";
@@ -7,7 +5,7 @@ import prettier from "prettier";
 import { OUTPUT_PATH, pkg, ROOT_PATH } from "./config";
 import task from "./util/task";
 
-const copyNewPackageVersion = async () => {
+export default task("copy-new-version", async () => {
   console.log("Start copying package.json version");
   const version = JSON.parse(fs.readFileSync(path.join(OUTPUT_PATH, "package.json"), "utf-8")).version;
   console.log(`Old version is ${pkg.version}`);
@@ -22,14 +20,4 @@ const copyNewPackageVersion = async () => {
     }),
   );
   console.log(`Done`);
-};
-
-const postpack = task("postpack", async () => {
-  console.log("postpack");
-  return copyNewPackageVersion();
-});
-
-export default task("postpublish", async () => {
-  console.log("postpublish");
-  return copyNewPackageVersion();
 });
