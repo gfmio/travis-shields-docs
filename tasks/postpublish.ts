9c9a988d281603a277bf57a56d8fa5e30325873d
Frederique Mittelstaedt <frederique@automorph.com>
2019-03-07 14:22:02 +0000

    build: add debug statements to postpublish/pack scripts

--- tasks/postpublish.ts
+++ tasks/postpublish.ts
@@ -7,8 +7,11 @@ import prettier from "prettier";
 import { OUTPUT_PATH, pkg, ROOT_PATH } from "./config";
 import task from "./util/task";
 
-export default task("postpublish", async () => {
+const copyNewPackageVersion = async () => {
+  console.log("Start copying package.json version");
   const version = JSON.parse(fs.readFileSync(path.join(OUTPUT_PATH, "package.json"), "utf-8")).version;
+  console.log(`Old version is ${pkg.version}`);
+  console.log(`New version is ${version}`);
   const updatedPackageJson = { ...pkg, version };
   fs.writeFileSync(
     path.join(ROOT_PATH, "package.json"),
@@ -18,4 +21,15 @@ export default task("postpublish", async () => {
       useTabs: false,
     }),
   );
+  console.log(`Done`);
+};
+
+const postpack = task("postpack", async () => {
+  console.log("postpack");
+  return copyNewPackageVersion();
+});
+
+export default task("postpublish", async () => {
+  console.log("postpublish");
+  return copyNewPackageVersion();
 });
